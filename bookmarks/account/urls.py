282854d352dc9261c16d761c9bb3c1c9c0a2689e
YassineLr <Yassine.lr.2001@gmail.com>
2024-11-09 20:25:41 +0100

    login

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    re_path(r'^login/$', view=auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    re_path(r'^$', view=views.dashboard, name='dashboard')
+]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_REDIRECT_URL = reverse_lazy('logout')
